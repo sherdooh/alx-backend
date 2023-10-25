1c611607ce5e17023c36a41a7cb1ed5557dfe2cd
ShadrachKiplangat <55084539+sherdooh@users.noreply.github.com>
2023-10-25 14:23:43 +0300

    simple_helper_function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,11 @@
-def index_range(page, page_size):
-    if page <= 0 or page_size <= 0:
-        return (0, 0)  # Invalid input, return an empty range
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+#!/usr/bin/env python3
+"""This is a simple helper function"""
 
-# Example usage:
-page = 3
-page_size = 10
-start, end = index_range(page, page_size)
-print(f"Start Index: {start}, End Index: {end}")
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns tuple of size two containing the start index and an end index rnage"""
+
+    return (page * page_size - page_size, page * page_size)
